c691211cedca11566dc0a5f263faf485b1a2bb0f
Uminosachi <49424133+Uminosachi@users.noreply.github.com>
2023-04-12 05:05:06 +0900

    Enlarged sketch area

--- iasam_app.py
+++ iasam_app.py
@@ -142,7 +142,7 @@ with block:
             out_image = gr.Image(label="Inpainted image", elem_id="out_image", interactive=False).style(height=480)
             
         with gr.Column():
-            sam_image = gr.Image(label="Segment Anything image", elem_id="sam_image", type="numpy", tool="sketch", brush_radius=6).style(height=480)
+            sam_image = gr.Image(label="Segment Anything image", elem_id="sam_image", type="numpy", tool="sketch", brush_radius=8).style(height=480)
             select_btn = gr.Button("Determine mask")
             
             sel_mask = gr.Image(label="Selected mask image", elem_id="sel_mask", type="numpy", interactive=False).style(height=480)
